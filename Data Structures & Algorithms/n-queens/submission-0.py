48fe922a9770d8d42fc8bc2b7990a7c55975ea1c
Rishika Sharma <147053821+rishika6-star@users.noreply.github.com>
2026-06-30 17:58:38 +0530

    Add: n-queens - submission-0

---
+++ Data Structures & Algorithms/n-queens/submission-0.py
@@ -0,0 +1,43 @@
+class Solution:
+    def solveNQueens(self, n):
+        res = []
+        board = [["."] * n for _ in range(n)]
+
+        cols = set()
+        diag1 = set()      # row-col
+        diag2 = set()      # row+col
+
+        def dfs(row):
+
+            if row == n:
+                copy = ["".join(r) for r in board]
+                res.append(copy)
+                return
+
+            for col in range(n):
+
+                if (
+                    col in cols or
+                    (row-col) in diag1 or
+                    (row+col) in diag2
+                ):
+                    continue
+
+                board[row][col] = "Q"
+
+                cols.add(col)
+                diag1.add(row-col)
+                diag2.add(row+col)
+
+                dfs(row+1)
+
+                board[row][col] = "."
+
+                cols.remove(col)
+                diag1.remove(row-col)
+                diag2.remove(row+col)
+
+        dfs(0)
+
+        return res
+        
